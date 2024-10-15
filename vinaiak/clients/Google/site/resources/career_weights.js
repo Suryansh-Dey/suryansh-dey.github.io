5082c24985e28367766c34036e11a2f28fb53b4c
Suryansh Dey <deysuryansh@gmail.com>
2024-10-15 21:59:37 +0530

    adding google  chatbot

---
+++ vinaiak/clients/Google/site/resources/career_weights.js
@@ -0,0 +1,262 @@
+export default {
+    "Management (Business Executive)": {
+        "RIASEC": {
+            "Enterprising": 14,
+            "Social": 8,
+            "Conventional": 8,
+            "Investigative": 4,
+            "Artistic": 3,
+            "Realistic": 3
+        },
+        "Aptitude Test": {
+            "Verbal": 8,
+            "Numerical": 6,
+            "Abstract": 6,
+            "Data Checking": 5,
+            "Spatial": 4,
+            "Mechanical": 3,
+            "Work Sample": 3
+        },
+        "Big 5": {
+            "Extraversion": 10,
+            "Conscientiousness": 6,
+            "Openness": 4,
+            "Agreeableness": 3,
+            "Neuroticism": 2
+        }
+    },
+    "Researcher (Scientist)": {
+        "RIASEC": {
+            "Investigative": 16,
+            "Realistic": 10,
+            "Conventional": 5,
+            "Social": 4,
+            "Artistic": 3,
+            "Enterprising": 2
+        },
+        "Aptitude Test": {
+            "Numerical": 9,
+            "Abstract": 8,
+            "Spatial": 6,
+            "Verbal": 5,
+            "Mechanical": 4,
+            "Data Checking": 2,
+            "Work Sample": 1
+        },
+        "Big 5": {
+            "Openness": 8,
+            "Conscientiousness": 6,
+            "Neuroticism": 4,
+            "Agreeableness": 4,
+            "Extraversion": 3
+        }
+    },
+    "Mechanical Engineer": {
+        "RIASEC": {
+            "Realistic": 15,
+            "Investigative": 10,
+            "Conventional": 6,
+            "Enterprising": 4,
+            "Social": 3,
+            "Artistic": 2
+        },
+        "Aptitude Test": {
+            "Mechanical": 10,
+            "Spatial": 7,
+            "Numerical": 6,
+            "Abstract": 5,
+            "Data Checking": 4,
+            "Verbal": 2,
+            "Work Sample": 1
+        },
+        "Big 5": {
+            "Conscientiousness": 8,
+            "Openness": 6,
+            "Neuroticism": 4,
+            "Extraversion": 4,
+            "Agreeableness": 3
+        }
+    },
+    "Politician": {
+        "RIASEC": {
+            "Enterprising": 12,
+            "Social": 10,
+            "Conventional": 6,
+            "Artistic": 5,
+            "Investigative": 4,
+            "Realistic": 3
+        },
+        "Aptitude Test": {
+            "Verbal": 9,
+            "Abstract": 7,
+            "Data Checking": 5,
+            "Numerical": 5,
+            "Work Sample": 4,
+            "Spatial": 3,
+            "Mechanical": 2
+        },
+        "Big 5": {
+            "Extraversion": 10,
+            "Agreeableness": 7,
+            "Conscientiousness": 5,
+            "Openness": 2,
+            "Neuroticism": 1
+        }
+    },
+    "Artist (Painter, Designer)": {
+        "RIASEC": {
+            "Artistic": 18,
+            "Investigative": 6,
+            "Realistic": 4,
+            "Social": 4,
+            "Enterprising": 4,
+            "Conventional": 4
+        },
+        "Aptitude Test": {
+            "Abstract": 9,
+            "Spatial": 8,
+            "Verbal": 5,
+            "Numerical": 4,
+            "Mechanical": 3,
+            "Data Checking": 3,
+            "Work Sample": 3
+        },
+        "Big 5": {
+            "Openness": 10,
+            "Extraversion": 6,
+            "Neuroticism": 4,
+            "Agreeableness": 3,
+            "Conscientiousness": 2
+        }
+    },
+    "Influencer (Social Media Content Creator)": {
+        "RIASEC": {
+            "Enterprising": 12,
+            "Artistic": 10,
+            "Social": 9,
+            "Investigative": 4,
+            "Conventional": 3,
+            "Realistic": 2
+        },
+        "Aptitude Test": {
+            "Verbal": 8,
+            "Abstract": 7,
+            "Spatial": 6,
+            "Data Checking": 5,
+            "Numerical": 3,
+            "Work Sample": 3,
+            "Mechanical": 3
+        },
+        "Big 5": {
+            "Extraversion": 12,
+            "Openness": 5,
+            "Agreeableness": 4,
+            "Neuroticism": 2,
+            "Conscientiousness": 2
+        }
+    },
+    "Content Creator (Writer, Blogger)": {
+        "RIASEC": {
+            "Artistic": 15,
+            "Social": 6,
+            "Enterprising": 5,
+            "Investigative": 5,
+            "Conventional": 4,
+            "Realistic": 2
+        },
+        "Aptitude Test": {
+            "Verbal": 9,
+            "Abstract": 8,
+            "Spatial": 5,
+            "Data Checking": 5,
+            "Numerical": 4,
+            "Mechanical": 2,
+            "Work Sample": 2
+        },
+        "Big 5": {
+            "Openness": 10,
+            "Conscientiousness": 5,
+            "Agreeableness": 5,
+            "Extraversion": 3,
+            "Neuroticism": 2
+        }
+    },
+    "Software Engineer": {
+        "RIASEC": {
+            "Investigative": 12,
+            "Realistic": 10,
+            "Conventional": 8,
+            "Enterprising": 4,
+            "Social": 3,
+            "Artistic": 3
+        },
+        "Aptitude Test": {
+            "Numerical": 9,
+            "Abstract": 8,
+            "Spatial": 6,
+            "Verbal": 5,
+            "Mechanical": 4,
+            "Data Checking": 2,
+            "Work Sample": 1
+        },
+        "Big 5": {
+            "Conscientiousness": 8,
+            "Openness": 6,
+            "Neuroticism": 4,
+            "Agreeableness": 4,
+            "Extraversion": 3
+        }
+    },
+    "Data Scientist": {
+        "RIASEC": {
+            "Investigative": 14,
+            "Realistic": 8,
+            "Conventional": 7,
+            "Social": 4,
+            "Enterprising": 3,
+            "Artistic": 2
+        },
+        "Aptitude Test": {
+            "Numerical": 9,
+            "Abstract": 9,
+            "Spatial": 6,
+            "Data Checking": 5,
+            "Verbal": 4,
+            "Mechanical": 1,
+            "Work Sample": 1
+        },
+        "Big 5": {
+            "Openness": 7,
+            "Conscientiousness": 7,
+            "Neuroticism": 4,
+            "Agreeableness": 3,
+            "Extraversion": 2
+        }
+    },
+    "Lawyer": {
+        "RIASEC": {
+            "Enterprising": 12,
+            "Social": 8,
+            "Investigative": 6,
+            "Conventional": 6,
+            "Realistic": 4,
+            "Artistic": 4
+        },
+        "Aptitude Test": {
+            "Verbal": 9,
+            "Abstract": 7,
+            "Data Checking": 5,
+            "Numerical": 4,
+            "Mechanical": 2,
+            "Work Sample": 2,
+            "Spatial": 1
+        },
+        "Big 5": {
+            "Conscientiousness": 8,
+            "Openness": 6,
+            "Extraversion": 5,
+            "Agreeableness": 3,
+            "Neuroticism": 3
+        }
+    }
+}
