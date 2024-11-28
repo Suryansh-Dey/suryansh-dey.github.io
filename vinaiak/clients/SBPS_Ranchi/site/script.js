5802e7635cafc4b9cf829b26e79fbe596d6e57ea
suryansh-dey <deysuryansh@gmail.com>
2024-11-28 08:43:18 +0530

    Changed SBPS loginIcon

--- vinaiak/clients/SBPS_Ranchi/site/script.js
+++ vinaiak/clients/SBPS_Ranchi/site/script.js
@@ -17,98 +17,33 @@ function addBot(targetElement) {
   {
     const styles = document.createElement("link");
     styles.rel = "stylesheet";
-    styles.href = "/vinaiak/clients/BIT_Mesra/site/styles.css";
+    styles.href = "/vinaiak/clients/SBPS_Ranchi/site/styles.css";
     document.head.appendChild(styles);
   }
   const loginIcon = document.createElement("div");
   loginIcon.id = "bot-loginIcon";
   loginIcon.innerHTML =
-    '\
-            <video muted disablePictureInPicture preload="auto" id="popup"><source src="https://suryansh-dey.github.io/vinaiak/clients/BIT_Mesra/site/resources/namaste.mp4" type="video/mp4">AI assistants</video>\
-            <video muted disablePictureInPicture preload="auto" id="looking" style="display:none"><source src="https://suryansh-dey.github.io/vinaiak/clients/BIT_Mesra/site/resources/Looking_Around.mp4" type="video/mp4">AI assistants</video>\
-            <video muted disablePictureInPicture preload="auto" id="jump" style="display:none"><source src="https://suryansh-dey.github.io/vinaiak/clients/BIT_Mesra/site/resources/Jump.mp4" type="video/mp4">AI assistants</video>\
-            <video muted disablePictureInPicture preload="auto" id="hover" style="display:none"><source src="https://suryansh-dey.github.io/vinaiak/clients/BIT_Mesra/site/resources/onHover.mp4" type="video/mp4">AI assistants</video>\
-            <video muted disablePictureInPicture preload="auto" id="click" style="display:none"><source src="https://suryansh-dey.github.io/vinaiak/clients/BIT_Mesra/site/resources/onClick.mp4" type="video/mp4">AI assistants</video>\
-';
+    '<img src="/vinaiak/clients/SBPS_Ranchi/site/resources/icon.gif" alt="AI assistants"</img>';
   targetElement.appendChild(loginIcon);
   let captchaScript = document.createElement("script");
   captchaScript.src =
     "https://www.google.com/recaptcha/enterprise.js?render=" + captchaKey;
   captchaScript.id = "captcha";
   targetElement.appendChild(captchaScript);
-  loginIcon.querySelector("#popup").play();
-  let startWaiting = true;
-  const video = loginIcon.querySelector("#popup");
-  video.onended = () => {
-    video.src =
-      "https://suryansh-dey.github.io/vinaiak/clients/BIT_Mesra/site/resources/popup.mp4";
-    video.play();
-    video.onended = () => {
-      startWaiting = false;
-    };
-  };
-  setInterval(() => {
-    if (startWaiting) {
-      startWaiting = false;
-      return;
-    }
-    if (loginIcon.querySelector("#popup"))
-      loginIcon.removeChild(loginIcon.querySelector("#popup"));
-    loginIcon.querySelector("#hover").style.display = "none";
-    loginIcon.querySelector("#click").style.display = "none";
-    let video = loginIcon.querySelector("video");
-    if (Math.random() < 0.6) {
-      video = loginIcon.querySelector("#looking");
-      loginIcon.querySelector("#jump").style.display = "none";
-    } else {
-      video = loginIcon.querySelector("#jump");
-      loginIcon.querySelector("#looking").style.display = "none";
-    }
-    video.style.display = "block";
-    video.play();
-  }, 5000);
-  document
-    .getElementById("bot-loginIcon")
-    .addEventListener("mouseenter", () => {
-      startWaiting = true;
-      if (loginIcon.querySelector("#popup"))
-        document
-          .getElementById("bot-loginIcon")
-          .removeChild(loginIcon.querySelector("#popup"));
-      loginIcon.querySelector("#looking").style.display = "none";
-      loginIcon.querySelector("#jump").style.display = "none";
-      loginIcon.querySelector("#hover").style.display = "block";
-      loginIcon.querySelector("#hover").play();
-    });
-  document
-    .getElementById("bot-loginIcon")
-    .addEventListener("mouseleave", () => {
-      startWaiting = false;
-    });
 
   loginIcon.onclick = () => {
-    startWaiting = true;
-    const onClick = loginIcon.querySelector("#click");
-    onClick.style.display = "block";
-    onClick.play();
-    onClick.onended = () => {
-      onClick.style.display = "none";
-      startWaiting = false;
-    };
     if (Bot.exists) {
+      Bot.openFrame();
       setTimeout(() => {
-        Bot.openFrame();
-        setTimeout(() => {
-          document.getElementById("bot-loginIcon").style.display = "none";
-        }, 400);
-      }, 600);
+        document.getElementById("bot-loginIcon").style.display = "none";
+      }, 400);
       return;
     } else {
       setTimeout(() => {
         document.getElementById("bot-loginIcon").style.display = "none";
         if (!Bot.loaded) frameNotOpened = true;
         else Bot.openFrame();
-      }, 2000);
+      }, 500);
     }
     let customCss = document.createElement("style");
     customCss.textContent = `
